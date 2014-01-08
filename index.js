389ddc9d67123af5fb37dbfc909431fa146a53a4
Thanh Dang <clgt@clgt.vn>
2014-01-08 18:07:18 +0700

    Update

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 /**
  * Main script
  */
-var html = require('./template'),
-	$	 = require('jquery');
-	
+var Emitter = require('emitter')
+
+var layout 	= require('./template'),
+	$	 	= require('jquery'),
+	_	 	= require('underscore');
+
+/**
+ * Sample bracket data
+ */
+var bracketData = {
+	games: [
+		// Round 1
+		[
+			[1,2], [3,4], [5,6]
+		],
+		// Round 2
+		[
+			[7], [8], [9], [10,11]
+		],
+		// Round 3
+		[
+			[], []
+		],
+		// Final round
+		[
+			[]
+		]
+	]
+};
+
 /**
  * Init bracket
  * @param  {[type]} size [description]
  * @return {[type]}      [description]
  */
-module.exports = function(size) {
-	document.write(html);
+module.exports = Bracket;
+
+function Bracket(size, el) {
+	if (!(this instanceof Bracket)) return new Bracket(size, el);
+
+	Emitter.call(this);
+
+	this.size = size;
+	this.$el = el !== undefined ? $(el) : $('body');
+
+	this.zenData();
+	this.zenLayout();
+	this.drawRounds();
 }
+
+Bracket.prototype = new Emitter;
 
 /**
  * Generate data
@@ -19,8 +59,22 @@ module.exports = function(size) {
  * @param  {[type]} size [description]
  * @return {[type]}      [description]
  */
-var zenData = function(size) {
-	// Return array of data
+var zenData = function() {
+	// Get min & max closet power of 2
+	var power = getMinAndMaxClosetPowerOf2(this.size);
+	// Calculate number of round
+	this.numberOfRound = powerOf2(power.max);
+	// Calculate number of games
+	this.numberOfGame = this.size - 1;
+	// Calculate number of byes
+	this.numberOfByes = power.max - this.size;
+	// Calculate number of first round game
+	this.numberOfFirstRoundGame = this.size - power.min;
+}
+
+
+var zenLayout = function() {
+	this.$el.html(layout);
 }
 
 /**
@@ -50,5 +104,48 @@ var drawABracket = function(type) {
  * @return {Void}       [description]
  */
 var drawARound = function(name, games) {
-	
+	this.$el.find('tr').append('<td></td>');
+}
+
+var drawRounds = function() {
+	var _this = this;
+	_.each(bracketData.games, function(games){
+		_this.drawARound('Round', games);
+	});
+}
+
+Bracket.prototype.zenData = zenData;
+Bracket.prototype.zenLayout = zenLayout;
+Bracket.prototype.drawAGame = drawAGame;
+Bracket.prototype.drawABracket = drawABracket;
+Bracket.prototype.drawARound = drawARound;
+Bracket.prototype.drawRounds = drawRounds;
+
+/**
+ * Get min and max closet power of 2
+ * @param  {[type]} n [description]
+ * @return {[type]}   [description]
+ */
+var getMinAndMaxClosetPowerOf2 = function(n) {
+	n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    
+    return {
+        max: n,
+        min: n - (n >> 1)
+    };
+}
+
+/**
+ * [powerOf2 description]
+ * @param  {[type]} n [description]
+ * @return {[type]}   [description]
+ */
+var powerOf2 = function(n) {
+	return Math.log(n) / Math.log(2);
 }
